98968259ef7ac4827835a1abcea3e9692f824e2a
Kortelange <kortejakob@gmail.com>
2024-03-05 08:53:07 +0100

    Refactor morse_code as a package.

---
+++ morse_code/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Morse Code',
+    version='0.1.0',
+    description='A simple program for translating text to Morse code.',
+    author='Jakob Lange',
+    author_email='jakob_korte@hotmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
