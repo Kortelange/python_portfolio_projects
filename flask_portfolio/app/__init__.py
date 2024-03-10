23c6945bc65171549c4154c9e737e4bba2b4de66
Kortelange <kortejakob@gmail.com>
2024-03-10 11:37:22 +0100

    Add tictactoe route.

--- flask_portfolio/app/__init__.py
+++ flask_portfolio/app/__init__.py
@@ -6,9 +6,10 @@ def create_app():
     
     # Configuration settings can go here
     app.config['SECRET_KEY'] = 'your_secret_key'
-    from .routes import main, morse_code
+    from .routes import main, morse_code, tic_tac_toe
 
     app.register_blueprint(main, url_prefix="/")
     app.register_blueprint(morse_code, url_prefix="/morse_code")
+    app.register_blueprint(tic_tac_toe, url_prefix="/tictactoe")
     
     return app
--- flask_portfolio/app/routes.py
+++ flask_portfolio/app/routes.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from morse_code import text_to_morse
 
+# Define blueprints
 main = Blueprint('main', __name__)
+morse_code = Blueprint('morse_code', __name__)
+tic_tac_toe = Blueprint('tic_tac_toe', __name__)
 
 @main.route("/")
 def home():
     return render_template('index.html')
 
-
-morse_code = Blueprint('morse_code', __name__)
 
 @morse_code.route("/")
 def home():
@@ -31,3 +32,8 @@ def translate_to_morse():
             translation=translation
             )
     return redirect(url_for('morse_code.home'))
+
+
+@tic_tac_toe.route("/")
+def home():
+    return render_template('tictactoe.html')
