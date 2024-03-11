553e846a553640cfd30f9feba4c0504c60a62e03
Kortelange <kortejakob@gmail.com>
2024-03-11 09:07:56 +0100

    Refactor tictactoe.js. Add 'Reset Game' button to tictactoe.

--- flask_portfolio/app/static/js/tictactoe.js
+++ flask_portfolio/app/static/js/tictactoe.js
@@ -31,29 +31,40 @@ function chooseStartingPlayer(player) {
     .then(response => response.json())
     .then(data => {
         document.querySelector('#choose-start-player').style.display = 'none';
-        document.querySelector('#instructions').style.display = "true";
-        document.querySelector('#instructions').textContent = `${data.player.toUpperCase()}'s turn`
+        document.querySelector('#instructions').style.display = 'block';
+        document.querySelector('#instructions').textContent = `${data.player.toUpperCase()}'s turn`;
+        document.querySelector('#reset-game').style.display = 'block';
         currentPlayer = data.player;
         gameOver = false;
     })
 }
 
+function postRequest(url, index) {
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ index: index, player: currentPlayer })
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(err => {throw err; });
+        }
+        return response.json();
+    });
+}
+
+function handleError(error) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = error.error;
+    errorMessage.style.display = 'block';
+}
+
 function placeBrick(index) {
     url = document.body.getAttribute('place-url')
     if (gameOver == false) {
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ index: index, player: currentPlayer })
-        })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => {throw err; });
-            }
-            return response.json();
-        })
+        postRequest(url, index)
         .then(data => {
             document.getElementById('error-message').style.display='none';
             const divElement = document.querySelector(`#cell${index}`);
@@ -68,30 +79,14 @@ function placeBrick(index) {
             currentPlayer = data.player;
             document.getElementById('instructions').textContent = `${currentPlayer.toUpperCase()}'s turn.`;
         })
-        .catch(error => {
-            const errorMessage = document.getElementById('error-message');
-            errorMessage.textContent = error.error;
-            errorMessage.style.display = 'block';
-        });
+        .catch(error => handleError(error));
     }
 }
 
 function moveBrick(index) {
     url = document.body.getAttribute('move-url')
     if (!gameOver) {
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ index: index, player: currentPlayer })
-        })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => {throw err; });
-            }
-            return response.json();
-        })
+        postRequest(url, index)
         .then(data => {
             document.getElementById('error-message').style.display='none';
             const divElement = document.querySelector(`#cell${index}`);
@@ -99,11 +94,6 @@ function moveBrick(index) {
             divElement.setAttribute('slot-occupied', 'false');
             document.getElementById('instructions').textContent = `${currentPlayer.toUpperCase()}'s to place.`;
         })
-        .catch(error => {
-            const errorMessage = document.getElementById('error-message');
-            errorMessage.textContent = error.error;
-            errorMessage.style.display = 'block';
-        });
+        .catch(error => handleError(error));
     }
 }
-
